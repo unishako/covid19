4214bcd73d0cc5908c36ca0addc5f15ce172db86
unishako <tmakn0822@icloud.com>
2021-06-05 12:54:57 +0900

    Update covid19.sql

--- src/test/resources/data/covid19.sql
+++ src/test/resources/data/covid19.sql
@@ -1,7 +1,4 @@
-USE covid19;
-INSERT INTO `covid19` (`yyyy_mm_dd`, `prefectures_code`, `prefectures_name`, `infection_person_count_one_day`,
-                       `infection_person_count_total`, `dead_count_one_day`, `dead_count_total`)
+INSERT INTO covid19
 VALUES ('9999-12-30', '99', '国国国国国国国国国１', 99991, 99992, 99993, 99994);
-INSERT INTO `covid19` (`yyyy_mm_dd`, `prefectures_code`, `prefectures_name`, `infection_person_count_one_day`,
-                       `infection_person_count_total`, `dead_count_one_day`, `dead_count_total`)
+INSERT INTO covid19
 VALUES ('9999-12-31', '99', '国国国国国国国国国２', 99995, 99996, 99997, 99998);
