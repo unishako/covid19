6f4fde7a0ecad4f5b29dd74e7622a0c11457ade1
unishako <tnishida0822@icloud.com>
2021-07-09 00:26:07 +0900

    Update FormatUtil.java

--- src/main/java/com/example/covid19/util/FormatUtil.java
+++ src/main/java/com/example/covid19/util/FormatUtil.java
@@ -6,11 +6,11 @@ public class FormatUtil {
     /**
      * カンマ編集
      *
-     * @param number {@link T} 数値
-     * @param <T>    数値型
+     * @param number {@link N} 数値
+     * @param <N>    数値型
      * @return カンマ編集済数値
      */
-    public static <T> String toComma(T number) {
+    public static <N extends Number> String toComma(N number) {
         NumberFormat nf = NumberFormat.getNumberInstance();
         return nf.format(number);
     }
