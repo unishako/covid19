7eae7ea9fb8c54f1daab2d1326f1de1eaaf13841
unishako <tnishida0822@icloud.com>
2022-09-04 22:02:35 +0900

    Update PrefecturesDaoTest.java

--- src/test/java/com/example/covid19/model/dao/PrefecturesDaoTest.java
+++ src/test/java/com/example/covid19/model/dao/PrefecturesDaoTest.java
@@ -45,7 +45,7 @@ class PrefecturesDaoTest {
     }
 
     @Test
-    @DataSet("/databaseRider/prefectures.csv")
+    @DataSet("databaseRider/prefectures.csv")
     @Transactional
     void findAll_DatabaseRider() {
         List<Prefectures> list = prefecturesDao.findAll();
